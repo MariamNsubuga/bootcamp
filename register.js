e6566fc395d24282c3e6550fe9c5c38de5ad5c15
MariamNsubuga <mariam.nakanyike@gmail.com>
2021-11-10 15:00:16 +0300

    register form

--- register.js
+++ register.js
@@ -1,69 +1,10 @@
-function validate() {
-    var Fname =
-        document.forms["RegForm"]["fname"];
-    var Uname =
-        document.forms["RegForm"]["uname"];
-    var Email =
-        document.forms["RegForm"]["email"];
-    var Tele =
-        document.forms["RegForm"]["tel"];
-    var Pass =
-        document.forms["RegForm"]["pwd"];
-    var Cpass =
-        document.forms["RegForm"]["cpwd"];
-    var Per =
-        document.forms["RegForm"]["person"];
 
+export default function validate(values) {
 
-    if (Fname.value == "") {
-       window.alert("Please enter your  Full Name.");
-      // document.getElementById('fname').innerHTML = "this is invalid name";
-       Fname.focus();
-       return false;
+    let errors = {};
+
+    if (!values.fname.trim()) {
+        errors.fname = 'Fullname required';
     }
-
-    if (Uname.value == "") {
-        alert("Please enter your  User Name.");
-        Uname.focus();
-        return false;
-    }
-    if (Tele.value == "") {
-        alert("Please enter your Telephone.");
-        Tele.focus();
-        return false;
-    }
-
-    if (Email.value == "") {
-        alert(
-            "Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (Per.value == "") {
-        alert(
-            "Please select an option.");
-        Per.focus();
-        return false;
-    }
-
-    if (Pass.value == "" || !Pass.value.match(/^.{5,15}$/)) {
-        alert("Please enter your password and it should be length must be between 5-15 characters!");
-        Pass.focus();
-        return false;
-    }
-
-    if (Cpass.value == "" || !Cpass.value.match(/^.{5,15}$/)) {
-        alert("Please confirm password");
-        Cpass.focus();
-        return false;
-    }
-
-    if (Pass.value != Cpass.value) {
-        alert("Password did not match");
-    }
-
-
-
-    return true;
+    return errors;
 }
